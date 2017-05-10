3213f7738279c9817475bae8c76e4c46dfb02b05
Vanessa Martinez <vmartinez.contact@gmail.com>
2017-05-09 17:02:08 -0700

    adds filters
    Thanks, Thomas.

--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,6 @@ var $ = require("./lib/qsa");
 var debounce = require("./lib/debounce");
 var scrollTo = require("./lib/animateScroll");
 
-var parseDate = function(str) {
-  var [month, day, year] = str.split("/").map(Number);
-  month -= 1;
-  return new Date(year, month, day);
-}
-
 var formatDate = function(date) {
   var months = [null, "Jan.", "Feb.", "Mar.", "Apr.", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."];
   var day = date.getDate();
@@ -21,45 +15,53 @@ var formatDate = function(date) {
 }
 
 // rebuild data from the DOM
-var rows = $(".fall-arts tbody.events tr").map(function(tr) {
+var rows = $(".summer-events tbody.events tr").map(function(tr) {
   var props = "event date location description".split(" ");
   var o = {};
   props.forEach(p => o[p] = tr.querySelector("." + p).innerHTML.trim().replace("&amp;", "&"));
   o.element = tr;
   o.category = tr.getAttribute("data-category");
-  o.start = parseDate(tr.getAttribute("data-start"));
-  if (tr.getAttribute("data-end")) o.end = parseDate(tr.getAttribute("data-end"));
-  tr.querySelector("span.start").innerHTML = formatDate(o.start);
-  if (o.end) tr.querySelector("span.end").innerHTML = formatDate(o.end);
+  o.dates = tr.querySelector(".date").innerHTML;
+  var [month] = o.dates.match(/thru|may|june|july|aug|sept/i) || [""];
+  month = month.toLowerCase();
+  o.month = month == "thru" ? "may" : month;
   o.pick = tr.classList.contains("pick");
   return o;
 });
 
 var catList = document.querySelector(".filters .categories");
+var monthList = document.querySelector(".filters .meses")
 var searchBox = document.querySelector(".filters .search");
-var table = document.querySelector(".fall-arts");
+var table = document.querySelector(".summer-events");
 var edPicks = document.querySelector(".editors");
 
 var filterByCategory = function(cats, list) {
   return list.filter(r => cats.indexOf(r.category) > -1);
 };
 
+var filterByMonth = function(meses, list) {
+  return list.filter(r => meses.indexOf(r.month) > -1);
+};
+
 var filterBySearch = function(q, list) {
+  if (!q) return list;
   var re = new RegExp(q, "i");
   return list.filter(r => r.event.match(re) || r.location.match(re) || r.description.match(re));
 };
 
-var filterForPicks = function(list) {
-  return list.filter(r => r.pick);
-};
 
 var chainFilters = function() {
-  var checked = $("input[type=checkbox]:checked", catList).map(el => el.getAttribute("data-category"));
+  var checked = $(".categories input[type=checkbox]:checked", catList).map(el => el.getAttribute("data-category"));
+  var mesChecked = $(".meses input[type=checkbox]:checked", monthList).map(el => el.getAttribute("data-month"));
   var query = searchBox.value;
-  var byCat = filterByCategory(checked, rows);
-  var byQ = query ? filterBySearch(query, byCat) : byCat;
-  var picks = edPicks.checked ? filterForPicks(byQ) : byQ;
-  return picks;
+  var filters = [
+    // { filter: filterByCategory, value: checked },
+    { filter: filterByMonth, value: mesChecked },
+    { filter: filterBySearch, value: query }
+  ];
+  var filtered = rows;
+  filters.forEach(def => filtered = def.filter(def.value, filtered));
+  return filtered;
 }
 
 var applyFilters = debounce(function() {
@@ -80,6 +82,8 @@ var applyFilters = debounce(function() {
 
 catList.addEventListener("click", applyFilters);
 
+monthList.addEventListener("click", applyFilters);
+
 searchBox.addEventListener("keyup", applyFilters);
 
 edPicks.addEventListener("click", applyFilters);
@@ -88,15 +92,15 @@ edPicks.addEventListener("click", applyFilters);
 
 
 //scroll to today's events
-document.querySelector(".jump-to-today").addEventListener("click", function() {
-  var today = new Date();
-  today.setHours(0);
-  today.setMinutes(0);
-  today.setSeconds(0);
-  var filtered = chainFilters();
-  for (var i = 0; i < filtered.length; i++) {
-    var row = filtered[i];
-    if (row.end >= today) return scrollTo(row.element);
-    if (row.start >= today) return scrollTo(row.element);
-  }
-});
+// document.querySelector(".jump-to-today").addEventListener("click", function() {
+//   var today = new Date();
+//   today.setHours(0);
+//   today.setMinutes(0);
+//   today.setSeconds(0);
+//   var filtered = chainFilters();
+//   for (var i = 0; i < filtered.length; i++) {
+//     var row = filtered[i];
+//     if (row.end >= today) return scrollTo(row.element);
+//     if (row.start >= today) return scrollTo(row.element);
+//   }
+// });
